80f18a7b25c47cef5954129d60faa0cb78c04fb2
cufardixx <facundopicia@gmail.com>
2023-05-02 19:30:37 -0300

    agregado de dotenv para que use el puerto que quiera

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 //lo q esta pasando es que nuestrea PC esta actuando de cliente y servidor!!!!!!!!!
-
+//requerrir libreria de dotenv
+require('dotenv').config()
+const { log } = require('console');
 //requerrir libreria HTTP server para obtener metodos de red
 const http = require('http');
+
+
 
 
 //funcion para el crateServer
@@ -13,5 +17,9 @@ function requestController(){
 const server = http.createServer(requestController)
 
 
+//variable de un puerto para que pueda correr en produccion en render.com
+const PORT = process.env.PORT
 //ahora falta que el servidor se ponga a en escucha en un purto determinado! no hace nada mas q escuchar porq no esta dovolviendo una respuesta en el servidor
-server.listen(4000)
+server.listen(PORT, ()=>{
+    console.log("Aplicacion corriendo en puerto " + PORT)
+})
